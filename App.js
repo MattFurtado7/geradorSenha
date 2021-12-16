16272edef79a9d4a874b131fddc64bafc626d017
Mepzer <matheus.furtado@aluno.uepa.br>
2021-12-16 13:15:01 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import {Image} from 'react-native';
+import styled from 'styled-components/native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilo.container}>
+      
+          <Image style={estilo.imagem} source = {require('./assets/lock.png')}
+            style = {{ width: 280, height: 180}}
+            resizeMode="cover"
+          />
+
+
+      <Text style={estilo.title}>Caracteres</Text>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#DDD',
+    justifyContent: 'center'
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30
+  },
+  input: {
+    backgroundColor: '#DDD',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#000',
+    fontSize: 23
+  },
+  botao: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41AEF4'
+  },
+  textoBotao: {
+    color: '#FFF',
+    fontSize: '25'
+  },
+  imagem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
